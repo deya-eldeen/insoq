d7d0869f58ceffe7aba8357490f8d8dd8af5d4cb
Deya <deya@Octopus.local>
2022-04-19 18:45:03 +0300

    updates

--- inSooqApp/API/Models/ListItem.swift
+++ inSooqApp/API/Models/ListItem.swift
@@ -10,8 +10,16 @@ import Foundation
 struct ListItem: Codable, Listable {
     
     let id:  Int?
+    
     let ar_Text:  String?
     let en_Text:  String?
     
+    let typeId: Int?
+    let categoryId: Int?
+    let emirateId: Int?
+    let operatorId: Int?
+    
+    let value:  String?
+
 }
 
